56149bd8fd263416d6b3b63d71813b1a66ca9edd
user9374525996 <3476033405197864-user9374525996@noreply.coze.cn>
2026-06-18 19:38:19 +0800

    chore: 调大 Splash Screen Logo 尺寸 (200 → 350)

--- app.config.ts
+++ app.config.ts
@@ -79,7 +79,7 @@ export default ({ config }: ConfigContext): ExpoConfig => {
         "expo-splash-screen",
         {
           "image": "./assets/images/splash-icon.png",
-          "imageWidth": 200,
+          "imageWidth": 350,
           "resizeMode": "contain",
           "backgroundColor": "#000000"
         }
